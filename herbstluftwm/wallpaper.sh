e03de382843cc070c802c318fcab52bc8a3c4910
Chance Zibolski <chance.zibolski@gmail.com>
2013-01-19 15:59:55 -0800

    Make general wallpaper folder

--- herbstluftwm/wallpaper.sh
+++ herbstluftwm/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-    find ~/Pictures/insomniac/ -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
+    find ~/Pictures/wallpapers/ -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
         shuf -n1 -z | xargs -0 feh --bg-max
     sleep 5m
 done
