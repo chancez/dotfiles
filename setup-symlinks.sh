f2ffa90908b42d5e45ef65fa9c5bac25d723d060
Chance Zibolski <chance.zibolski@gmail.com>
2021-12-23 17:25:34 -0800

    Update setup-symlinks.sh

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -6,31 +6,18 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 git submodule update --init --recursive
 
-mkdir -p "$HOME/.vim/tmp/backup"
-mkdir -p "$HOME/.vim/tmp/swap"
-mkdir -p "$HOME/.vim/tmp/undo"
-ln -s "$DOTFILES_DIR/vimrc" "$HOME/.vimrc"
-mkdir -p "$HOME/.config/nvim"
-ln -s "$DOTFILES_DIR/init.vim" "$HOME/.config/nvim/init.vim"
+STOW_DIRS=(\
+    ag \
+    bins \
+    ctags \
+    git \
+    neovim \
+    postgres \
+    ssh \
+    tmux \
+    zsh \
+)
 
-ln -s "$DOTFILES_DIR/tmux.conf" "$HOME/.tmux.conf"
-
-ln -s "$DOTFILES_DIR/ctags" "$HOME/.ctags"
-mkdir -p "$HOME/.ctags.d"
-ln -s "$DOTFILES_DIR/ctags" "$HOME/.ctags.d/default.ctags"
-
-ln -s "$DOTFILES_DIR/agignore" "$HOME/.agignore"
-ln -s "$DOTFILES_DIR/psqlrc" "$HOME/.psqlrc"
-ln -s "$DOTFILES_DIR/pgclirc" "$HOME/.pgclirc"
-ln -s "$DOTFILES_DIR/inputrc" "$HOME/.inputrc"
-ln -s "$DOTFILES_DIR/gitconfig" "$HOME/.gitconfig"
-ln -s "$DOTFILES_DIR/gitignore" "$HOME/.gitignore"
-ln -s "$DOTFILES_DIR/ssh_config" "$HOME/.ssh/config"
-
-if [[ ! -e "$HOME/.zprezto" ]]; then
-    ln -s "$DOTFILES_DIR/prezto" "$HOME/.zprezto"
-fi
-
-for rcfile in "${HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${HOME}/.${rcfile:t}"
+for dir in "${STOW_DIRS}"; do
+    stow -R "${dir}"
 done
