772186a78972a3c25db9b4968f994fca5b5c7978
Chance Zibolski <chance.zibolski@gmail.com>
2022-01-17 20:43:37 -0800

    Use mapx for all bindings

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -89,6 +89,10 @@ local packer = require('packer').startup(function(use)
 end)
 if not packer_exists then packer.sync() end -- install on first run
 
+-- Load mapx and make it available
+require'mapx'.setup { whichkey = true }
+local mapx = require'mapx'
+
 -- misc global opts
 vim.opt.mouse = 'a'
 vim.opt.colorcolumn = '80,100'
@@ -153,28 +157,31 @@ end
 -- language server
 
 -- Diagnostic keymaps
-vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
+mapx.group("silent", function()
+  mapx.nnoremap('<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>')
+  mapx.nnoremap('[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+  mapx.nnoremap(']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+  mapx.nnoremap('<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>')
+end)
 
 -- LSP settings
 local lspconfig = require 'lspconfig'
-local on_attach = function(_, bufnr)
-  local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
+local on_attach = function()
+  mapx.group("silent", "buffer", function()
+    mapx.nnoremap('gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+    mapx.nnoremap('gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+    mapx.nnoremap('K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+    mapx.nnoremap('gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+    mapx.nnoremap('<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+    mapx.nnoremap('<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
+    mapx.nnoremap('<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
+    mapx.nnoremap('<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
+    mapx.nnoremap('<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+    mapx.nnoremap('<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+    mapx.nnoremap('gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+    mapx.nnoremap('<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+    mapx.nnoremap('<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]])
+  end)
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
@@ -320,8 +327,6 @@ cmp.setup.cmdline(':', {
 })
 
 -- mappings
-require'mapx'.setup { whichkey = true }
-local mapx = require'mapx'
 
 -- Whichkey
 mapx.nmap('<leader>w', ':WhichKey<CR>', 'silent', 'Open WhichKey')
