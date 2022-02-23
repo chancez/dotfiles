95abe2bc3e98b478231530d8c54a9e89c2afca53
Chance Zibolski <chance.zibolski@gmail.com>
2022-02-23 10:08:13 -0800

    setup.sh: update for zgen and find stow packages automatically using find

--- setup.sh
+++ setup.sh
@@ -1,25 +1,14 @@
 #!/bin/zsh
+set -e
 setopt EXTENDED_GLOB
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-git submodule update --init --recursive
+if [[ ! -d "${HOME}/.zgen" ]]; then
+  echo "Installing zgen"
+  git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+fi
 
-STOW_DIRS=(\
-    ag \
-    brew \
-    bins \
-    ctags \
-    git \
-    kitty \
-    neovim \
-    postgres \
-    rg \
-    ssh \
-    tmux \
-    zsh \
-)
-
-for dir in "${STOW_DIRS[@]}"; do
+find . -depth 1 -type d -not -path './.git' |  cut -d/ -f2- | sort -h | while read -r dir; do
     stow -R "${dir}" 2>&1 | grep -v "BUG in find_stowed_path"
 done
