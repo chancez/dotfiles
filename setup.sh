4d75c30deceb8a0d06d65b88fdf5fcec342401f8
Chance Zibolski <chance.zibolski@gmail.com>
2022-02-23 10:20:43 -0800

    setup.sh: Remove zgen install

--- setup.sh
+++ setup.sh
@@ -4,11 +4,6 @@ setopt EXTENDED_GLOB
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-if [[ ! -d "${HOME}/.zgen" ]]; then
-  echo "Installing zgen"
-  git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
-fi
-
 find . -depth 1 -type d -not -path './.git' |  cut -d/ -f2- | sort -h | while read -r dir; do
     stow -R "${dir}" 2>&1 | grep -v "BUG in find_stowed_path"
 done
