3d754a805038ca786a6d5e167ea994b4f63c3e11
Chance Zibolski <chance.zibolski@gmail.com>
2022-02-23 14:13:08 -0800

    Add hammerspoon with VimMode.spoon

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,12 @@
+local VimMode = hs.loadSpoon('VimMode')
+local vim = VimMode:new()
+
+vim
+  :disableForApp('Code')
+  :disableForApp('MacVim')
+  :disableForApp('kitty')
+  :disableForApp('iTerm2')
+  :disableForApp('zoom.us')
+  :enterWithSequence('jk')
+  :bindHotKeys({ enter = { {'ctrl'}, ';'} })
+  -- :bindHotKeys({ enter = { {}, 'escape'} })
