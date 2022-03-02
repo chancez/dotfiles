b91afe79ec84e9726f673d3472d7ac5851486d2b
Chance Zibolski <chance.zibolski@gmail.com>
2022-03-01 16:25:43 -0800

    kitty: Fix relative resize when a window has neighbors in both directions relative to the resize direction

--- kitty/.config/kitty/relative_resize.py
+++ kitty/.config/kitty/relative_resize.py
@@ -11,22 +11,58 @@ def handle_result(args, result, target_window_id, boss):
 
     direction = args[1]
 
-    neighbor_direction = direction
-    if direction == 'up':
-        neighbor_direction = 'top'
-    if direction == 'down':
-        neighbor_direction = 'bottom'
+    neighbors = boss.active_tab.current_layout.neighbors_for_window(window, boss.active_tab.windows)
+    current_window_id = boss.active_tab.active_window
 
-    has_neighbor = boss.active_tab.neighboring_group_id(neighbor_direction) is not None
+    left_neighbors = neighbors.get('left')
+    right_neighbors = neighbors.get('right')
+    top_neighbors = neighbors.get('top')
+    bottom_neighbors = neighbors.get('bottom')
 
-    if direction == 'left' or direction == 'right':
-        if has_neighbor:
-            boss.active_tab.resize_window('wider', 1)
-        else:
-            boss.active_tab.resize_window('narrower', 1)
+    # has a neighbor on both sides
+    if direction == 'left' and (left_neighbors and right_neighbors):
+        # boss.active_tab.set_active_window(left_neighbors[0])
+        boss.active_tab.resize_window('narrower', 1)
+        # boss.active_tab.set_active_window(current_window_id)
+    # only has left neighbor
+    elif direction == 'left' and left_neighbors:
+        boss.active_tab.resize_window('wider', 1)
+    # only has right neighbor
+    elif direction == 'left' and right_neighbors:
+        boss.active_tab.resize_window('narrower', 1)
 
-    if direction == 'up' or direction == 'down':
-        if has_neighbor:
-            boss.active_tab.resize_window('taller', 1)
-        else:
-            boss.active_tab.resize_window('shorter', 1)
+    # has a neighbor on both sides
+    elif direction == 'right' and (left_neighbors and right_neighbors):
+        # boss.active_tab.set_active_window(left_neighbors[0])
+        boss.active_tab.resize_window('wider', 1)
+        # boss.active_tab.set_active_window(current_window_id)
+    # only has left neighbor
+    elif direction == 'right' and left_neighbors:
+        boss.active_tab.resize_window('narrower', 1)
+    # only has right neighbor
+    elif direction == 'right' and right_neighbors:
+        boss.active_tab.resize_window('wider', 1)
+
+    # has a neighbor above and below
+    elif direction == 'up' and (top_neighbors and bottom_neighbors):
+        # boss.active_tab.set_active_window(top_neighbors[0])
+        boss.active_tab.resize_window('shorter', 1)
+        # boss.active_tab.set_active_window(current_window_id)
+    # only has top neighbor
+    elif direction == 'up' and top_neighbors:
+        boss.active_tab.resize_window('taller', 1)
+    # only has bottom neighbor
+    elif direction == 'up' and bottom_neighbors:
+        boss.active_tab.resize_window('short', 1)
+
+    # has a neighbor above and below
+    elif direction == 'down' and (top_neighbors and bottom_neighbors):
+        # boss.active_tab.set_active_window(top_neighbors[0])
+        boss.active_tab.resize_window('taller', 1)
+        # boss.active_tab.set_active_window(current_window_id)
+    # only has top neighbor
+    elif direction == 'down' and top_neighbors:
+        boss.active_tab.resize_window('short', 1)
+    # only has bottom neighbor
+    elif direction == 'down' and bottom_neighbors:
+        boss.active_tab.resize_window('taller', 1)
