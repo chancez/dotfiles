c44687c6a25fdafe1d7b2136fda8db05770d7a26
Chance Zibolski <chance.zibolski@gmail.com>
2022-03-04 08:52:07 -0800

    Remove telescope tags binding (use LSP dynamic_workspace_symbols) and add telescope command history binding

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -632,9 +632,10 @@ require('telescope').load_extension('fzf')
 
 mapx.nnoremap('<c-p>', "<cmd>lua require('telescope.builtin').find_files()<cr>", 'Telescope find_files')
 mapx.nnoremap('<m-o>', "<cmd>lua require('telescope.builtin').buffers()<cr>", 'Telescope buffers')
-mapx.nnoremap('<m-p>', "<cmd>lua require('telescope.builtin').tags()<cr>", 'Telescope tags')
+-- mapx.nnoremap('<m-p>', "<cmd>lua require('telescope.builtin').tags()<cr>", 'Telescope tags')
 mapx.nnoremap('<c-_>', "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>", 'Telescope current_buffer_fuzzy_find') -- ctrl-/
 mapx.nnoremap('<c-g>', "<cmd>lua require('telescope.builtin').grep_string()<cr>", 'Telescope grep_string')
+mapx.nnoremap('<m-;>', "<cmd>lua require('telescope.builtin').command_history()<cr>", 'Telescope command_history')
 
 mapx.nnoremap('<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>", 'Telescope find_files')
 mapx.nnoremap('<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>", 'Telescope live_grep')
