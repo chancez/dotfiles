257506b53aea0be27ec86f10a7018c7c617df4a2
Chance Zibolski <chance.zibolski@gmail.com>
2022-03-07 10:40:27 -0800

    neovim: Add node_host_prog to asdf shim

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -209,6 +209,7 @@ vim.g.mapleader = ','
 
 vim.g.python_host_prog = '~/.asdf/shims/python2'
 vim.g.python3_host_prog = '~/.asdf/shims/python3'
+vim.g.node_host_prog = '~/.asdf/shims/node'
 
 -- language server
 
