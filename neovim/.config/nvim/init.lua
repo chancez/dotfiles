28f03e04438b629299fce2ed2e878d49cecf6c1b
Chance Zibolski <chance.zibolski@gmail.com>
2022-05-31 14:08:28 -0700

    Configure treesitter folding & fold keybindings

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -155,7 +155,6 @@ vim.opt.colorcolumn = '80,100'
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
 vim.opt.completeopt = 'menu,menuone,longest,noinsert,noselect'
-vim.opt.foldopen = 'block,insert,jump,mark,percent,quickfix,search,tag,undo'
 vim.opt.autoread = true
 vim.opt.hidden = true
 vim.opt.scrolloff = 5 -- Begin scrolling when cursor is at 5 from the edge
@@ -191,6 +190,16 @@ vim.opt.grepprg = 'rg --vimgrep --no-heading --smart-case'
 vim.opt.signcolumn = 'yes'
 -- Look for tags in the project directory
 vim.opt.tags = 'tags;'
+
+-- folds
+--Specifies for which type of commands folds will be opened, if the
+vim.opt.foldopen = 'block,insert,jump,mark,percent,quickfix,search,tag,undo'
+vim.opt.foldenable = true
+vim.opt.foldlevelstart = 10 -- open most folds by default
+vim.opt.foldnestmax = 10 -- 10 nested fold max
+mapx.nnoremap('<space>', 'za', 'Toggle folds')
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- indent-blankline
 vim.opt.list = true
