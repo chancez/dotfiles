519e71efe0f5762cb01ae9b8c889d98ea91ab619
Chance Zibolski <chance.zibolski@gmail.com>
2022-06-20 21:37:27 +0000

    Use find -maxdepth

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ setopt EXTENDED_GLOB
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-find . -depth 1 -type d -not -path './.git' |  cut -d/ -f2- | sort -h | while read -r dir; do
-    stow -R "${dir}" 2>&1 | grep -v "BUG in find_stowed_path"
+find . -maxdepth 1 -type d -not -path './.git' -not -path . | cut -d/ -f2- | sort -h | while read -r dir; do
+    stow -v "${dir}" 2>&1 | grep -v "BUG in find_stowed_path"
 done
