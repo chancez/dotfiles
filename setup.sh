af5dcf6a8278b38275a706a080ff96ab2ab61c3b
Chance Zibolski <chance.zibolski@gmail.com>
2023-03-01 15:15:27 -0800

    setup.sh: Use stow --no-folding

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ setopt EXTENDED_GLOB
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 find . -maxdepth 1 -type d -not -path './.git' -not -path . | cut -d/ -f2- | sort -h | while read -r dir; do
-    stow -v "${dir}" 2>&1 | grep -v "BUG in find_stowed_path"
+    stow --no-folding -v "${dir}" 2>&1 | grep -v "BUG in find_stowed_path"
 done
