9da085c8feebacf859cf528d6bb3ee7817eeaecd
Chance Zibolski <chance.zibolski@gmail.com>
2023-10-23 09:04:30 -0700

    neovim: Update indent_blankline config

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -770,11 +770,11 @@ require'lualine'.setup {
     lualine_z = {'tabs'}
   }
 }
-
-require("indent_blankline").setup {
-  space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = false,
+require("ibl").setup {
+  -- indent = { char = " " },
+  -- space_char_blankline = " ",
+  -- show_current_context = true,
+  -- show_current_context_start = false,
 }
 
 require('marks').setup {
