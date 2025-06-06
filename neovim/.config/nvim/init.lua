b24f33c24643aa381faa409c95093ab8d2ae3c17
Chance Zibolski <chance.zibolski@gmail.com>
2025-06-06 13:39:43 -0700

    neovim: Try codecompanion.nvim for ai integration

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -106,6 +106,47 @@ require("lazy").setup({
 
     -- AI
     { 'zbirenbaum/copilot.lua' },
+    {
+      "olimorris/codecompanion.nvim",
+      opts = {
+        strategies = {
+          chat = {
+            adapter = "copilot",
+          },
+          inline = {
+            adapter = "copilot",
+          },
+          cmd = {
+            adapter = "copilot",
+          },
+        },
+        extensions = {
+          mcphub = {
+            callback = "mcphub.extensions.codecompanion",
+            opts = {
+              make_vars = true,
+              make_slash_commands = true,
+              show_result_in_chat = true
+            }
+          }
+        }
+      },
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        {
+          "ravitemer/mcphub.nvim",
+          build = "npm install -g mcp-hub@latest",  -- Installs `mcp-hub` node binary globally
+          opts = {
+            log = {
+              level = vim.log.levels.DEBUG,
+              to_file = true,
+              file_path = vim.fn.expand("~/mcphub.log"),
+            }
+          },
+        },
+      },
+    },
 
     {
       'L3MON4D3/LuaSnip',
