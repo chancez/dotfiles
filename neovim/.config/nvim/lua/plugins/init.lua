3ea92ac75bca95aad5f26df09518148358183239
Chance Zibolski <chance.zibolski@gmail.com>
2025-06-10 11:27:32 -0700

    neovim: Move AI stuff to separate spec file

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -62,64 +62,6 @@ return {
   },
   { 'chentoast/marks.nvim', event = "VeryLazy", opts = { default_mappings = false } },
 
-  -- AI
-  {
-    'zbirenbaum/copilot.lua',
-    cmd = "Copilot",
-    event = "InsertEnter",
-    opts = {
-      -- disable suggestions and panel since we're using cmp
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      filetypes = {
-        yaml = true,
-      },
-      copilot_node_command = os.getenv('HOME') .. '/.local/bin/mise-node-lts.sh',
-      server = {
-        type = 'nodejs',
-        custom_server_filepath = vim.fn.expand("~/.local/share/nvim/mason/bin/copilot-language-server"),
-      },
-    }
-  },
-
-  {
-    "olimorris/codecompanion.nvim",
-    cmd = {"CodeCompanion", "CodeCompanionChat", "CodeCompanionCmd", "CodeCompanionActions"},
-    opts = {
-      strategies = {
-        chat = {
-          adapter = "copilot",
-        },
-        inline = {
-          adapter = "copilot",
-        },
-        cmd = {
-          adapter = "copilot",
-        },
-      },
-      extensions = {
-        mcphub = {
-          callback = "mcphub.extensions.codecompanion",
-          opts = {
-            make_vars = true,
-            make_slash_commands = true,
-            show_result_in_chat = true
-          }
-        }
-      }
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      {
-        "ravitemer/mcphub.nvim",
-        opts = {
-          cmd = 'mcp-hub'
-        }
-      },
-    },
-  },
-
   {
     'L3MON4D3/LuaSnip',
     config = function()
