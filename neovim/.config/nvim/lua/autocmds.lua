e5bb3713faed4e65e429569a0c579767d09ad82e
Chance Zibolski <chance.zibolski@gmail.com>
2025-10-10 16:19:21 -0700

    neovim: Convert HighlightWhitespace fully to lua

--- neovim/.config/nvim/lua/autocmds.lua
+++ neovim/.config/nvim/lua/autocmds.lua
@@ -1,13 +1,29 @@
 local util = require 'util'
 
+local event_to_pattern = {
+  -- highlight trailing whitespace when we enter into a buffer
+  BufWinEnter = "\\s\\+$",
+  -- do not highlight trailing whitespace while inserting
+  InsertEnter = "\\s\\+\\%#\\@<!$",
+  -- re-highlight trailing whitespace when leaving insert mode
+  InsertLeave = "\\s\\+$",
+}
+
+vim.api.nvim_set_hl(0, 'ExtraWhitespace', { bg = 'red' })
+
 function HighlightWhitespace()
-  vim.cmd [[
-    highlight ExtraWhitespace guibg=red
-    match ExtraWhitespace /\s\+$/
-    autocmd BufWinEnter <buffer> match ExtraWhitespace /\s\+$/
-    autocmd InsertEnter <buffer> match ExtraWhitespace /\s\+\%#\@<!$/
-    autocmd InsertLeave <buffer> match ExtraWhitespace /\s\+$/
-  ]]
+  for event, pattern in pairs(event_to_pattern) do
+    vim.api.nvim_create_autocmd(event, {
+      buffer = 0,
+      callback = function()
+        -- Delete the existing match if it exists, we only want one at a time
+        if vim.w.whitespace_match then
+          vim.fn.matchdelete(vim.w.whitespace_match)
+        end
+        vim.w.whitespace_match = vim.fn.matchadd('ExtraWhitespace', pattern)
+      end,
+    })
+  end
 end
 
 local whitespaceIgnoreFileTypes = {
@@ -62,7 +78,6 @@ vim.api.nvim_create_user_command('ToggleStripTrailingWhitespace', ToggleStripTra
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
   pattern = '*',
   callback = function()
-    -- Only strip if the b:noStripWhitespace variable isn't set
     if not vim.b.noStripWhitespace then
       StripTrailingWhitespace()
     end
@@ -73,6 +88,6 @@ vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
 vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
   pattern = '*',
   callback = function()
-    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 300 })
+    vim.hl.on_yank({ higroup = 'IncSearch', timeout = 300 })
   end,
 })
