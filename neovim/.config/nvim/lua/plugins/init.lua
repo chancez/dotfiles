6f768b1e2a8efcb62a7231e443171bd95dc69e6c
Chance Zibolski <chance.zibolski@gmail.com>
2025-10-27 10:18:13 -0700

    neovim: Remove incline.nvim plugin and go back to per-buffer statusline setup

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -19,10 +19,7 @@ return {
     event = "VeryLazy",
     dependencies = { 'kyazdani42/nvim-web-devicons', lazy = true },
     opts = {
-      options = {
-        theme = 'onedark',
-        globalstatus = true,
-      },
+      options = { theme = 'onedark' },
       extensions = { 'toggleterm' },
       sections = {
         lualine_a = { 'mode', 'g:viewport_active_mode' },
@@ -51,46 +48,6 @@ return {
         lualine_z = { 'tabs' }
       }
     }
-  },
-  {
-    'b0o/incline.nvim',
-    dependencies = { 'kyazdani42/nvim-web-devicons', lazy = true },
-    event = 'VeryLazy',
-    opts = {
-      window = {
-        padding = 0,
-        margin = { horizontal = 0 },
-        placement = {
-          horizontal = "right",
-          vertical = "top"
-        },
-        winhighlight = {
-          active = {
-            Normal = "StatusLine",
-          },
-          inactive = {
-            Normal = "StatusLineNC",
-          }
-        },
-      },
-      render = function(props)
-        local helpers = require 'incline.helpers'
-        local devicons = require 'nvim-web-devicons'
-        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
-        if filename == '' then
-          filename = '[No Name]'
-        end
-        local ft_icon, ft_color = devicons.get_icon_color(filename)
-        local modified = vim.bo[props.buf].modified
-        return {
-          ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
-          ' ',
-          { filename, gui = modified and 'bold,italic' or 'bold' },
-          ' ',
-          guibg = '#44406e',
-        }
-      end,
-    },
   },
 
   {
