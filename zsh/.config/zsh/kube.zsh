3394a5f8b8b1d185065218545c554a9e151622b7
Chance Zibolski <chance.zibolski@gmail.com>
2025-11-25 10:10:46 -0800

    zsh: Move kubectl fzf completions to kube.zsh

--- zsh/.config/zsh/kube.zsh
+++ zsh/.config/zsh/kube.zsh
@@ -110,3 +110,93 @@ kube-get-pod-resources-missing() {
       ' |
     column -t -s $'\t'
 }
+
+__extract_kubectl_namespace_from_args() {
+  local namespace namespace_args_index
+  namespace="default"
+
+  # Get the index of the --namespace or -n argument if provided
+  namespace_args_index=$(_args_index namespace "$@" || _args_index -n "$@")
+
+  if [[ -n "$namespace_args_index" ]]; then
+    namespace="${args[$namespace_args_index + 1]}"
+  fi
+
+  echo $namespace
+}
+
+__kubectl_get_pods() {
+  local namespace=${1:?}
+  kubectl --namespace="${namespace}" get pods -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n'
+}
+
+__kubectl_get_services() {
+  local namespace=${1:?}
+  kubectl --namespace="${namespace}" get services -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n'
+}
+
+__helper_fzf_complete_kubectl_pods() {
+  local namespace=${1:?}
+  shift
+
+  # fuzzy complete pod names for logs command
+  _fzf_complete --prompt="pod> " -- "$@" < <(__kubectl_get_pods "${namespace}")
+}
+
+__helper_fzf_complete_kubectl_namespaces() {
+  _fzf_complete --prompt="namespace> " -- "$@" < <(
+    kubectl get namespaces -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n'
+  )
+}
+
+__helper_fzf_complete_kubectl_port_forward_target() {
+  local namespace=${1:?}
+  shift
+
+  local pods services
+
+  services=$(__kubectl_get_services "${namespace}" | awk '{print "service/" $0}')
+  pods=$(__kubectl_get_pods "${namespace}" | awk '{print "pod/" $0}')
+
+  # fuzzy complete pods and service name for port-forward command
+  _fzf_complete --prompt="service and pods> " -- "$@" < <(
+    echo "${services}"
+    echo "${pods}"
+  )
+}
+
+__helper_fzf_complete_kubectl() {
+  local args namespace last_arg
+  args=(${(z)1})
+  last_arg="${args[-1]}"
+  namespace=$(__extract_kubectl_namespace_from_args ${args[@]})
+
+  if [[ "${last_arg}" == "--namespace" || "${last_arg}" == "-n" ]]; then
+    __helper_fzf_complete_kubectl_namespaces "$@"
+  elif [[ "${last_arg}" == "--context" ]]; then
+    _fzf_complete --prompt="context> " -- "$@" < <(
+      kubectl config get-contexts -o name
+    )
+  elif _args_contains logs "${args[@]}"; then
+    __helper_fzf_complete_kubectl_pods $namespace "$@"
+  elif _args_contains port-forward "${args[@]}"; then
+    __helper_fzf_complete_kubectl_port_forward_target $namespace "$@"
+  elif _args_contains pod "${args[@]}"; then
+    if _args_contains get "${args[@]}" || _args_contains describe "${args[@]}" || _args_contains delete "${args[@]}" ; then
+      __helper_fzf_complete_kubectl_pods $namespace "$@"
+    fi
+  elif [[ ${args[(ie)create]} -le ${#args} && ("${last_arg}" == "-f" || "${last_arg}" == "--filename") ]]; then # if create and -f/--filename
+    _fzf_path_completion "$prefix" "$@"
+  fi
+}
+
+# shellcheck disable=all
+{
+  # Define fzf completion functions for kubectl, k, and kubecolor
+  for cmd in kubectl k kubecolor; do
+    _fzf_complete_${cmd}() {
+      __helper_fzf_complete_kubectl "$@"
+    }
+  done
+}
+
