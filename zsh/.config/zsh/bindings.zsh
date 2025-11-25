a85e3876559e72cd94c8b418db5e05639c42fbab
Chance Zibolski <chance.zibolski@gmail.com>
2025-11-25 14:17:13 -0800

    zsh: .config/zsh/bindings.zsh: fix cmd_to_clip to not add a trailing newline

--- zsh/.config/zsh/bindings.zsh
+++ zsh/.config/zsh/bindings.zsh
@@ -6,8 +6,8 @@ zmodload zsh/terminfo
 # open the currently entered command in a text editor using 'v' in normal mode
 bindkey -M vicmd v edit-command-line
 
-# Copy the current command line to clipboard with Ctrl-Y
-cmd_to_clip() { pbcopy <<< ${BUFFER} }
+# Copy the current command line to clipboard with Ctrl-Y (without the trailing newline)
+cmd_to_clip() { print -n -- "$BUFFER" | pbcopy }
 zle -N cmd_to_clip
 bindkey '^Y' cmd_to_clip
 
