c140f0ab7441d79e7de3b62826e11898d5a456e8
Chance Zibolski <chance.zibolski@gmail.com>
2025-11-25 11:17:09 -0800

    Replace iterative fzf cd with jump top | fzf

--- zsh/.config/zsh/fzf.zsh
+++ zsh/.config/zsh/fzf.zsh
@@ -48,25 +48,9 @@ _fzf_complete_gco() {
 
 # fzf based cd without args
 cd() {
-    if [[ "$#" != 0 ]]; then
-        builtin cd "$@";
-        return
-    fi
-    while true; do
-        local lsd=$(echo ".." && ls -p | grep '/$' | sed 's;/$;;')
-        local dir="$(printf '%s\n' "${lsd[@]}" |
-            fzf --reverse --preview '
-                __cd_nxt="$(echo {})";
-                __cd_path="$(echo $(pwd)/${__cd_nxt} | sed "s;//;/;")";
-                echo $__cd_path;
-                echo;
-                ls -p --color=always "${__cd_path}";
-        ')"
-        [[ ${#dir} != 0 ]] || return 0
-        builtin cd "$dir" &> /dev/null
-    done
-}
-
-j() {
+  if [[ "$#" != 0 ]]; then
+      builtin cd "$@";
+      return
+  fi
   cd "$(jump top | fzf --reverse)"
 }
