3ecdd50c0d1afbae24418bf3c23ee423edf1f2eb
Chance Zibolski <chance.zibolski@gmail.com>
2025-12-01 11:35:01 -0800

    neovim: Remove render-markdown plugin

--- neovim/.config/nvim/lua/plugins/ai.lua
+++ neovim/.config/nvim/lua/plugins/ai.lua
@@ -123,14 +123,6 @@ return {
       "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
       "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
       "zbirenbaum/copilot.lua",        -- for providers='copilot'
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
     },
   },
 }
