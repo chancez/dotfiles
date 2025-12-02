002fe4caf923e57044c20c7ac8d61612c9eefc23
Chance Zibolski <chance.zibolski@gmail.com>
2025-12-02 11:51:18 -0800

    neovim: Disable file_browser hijack_netrw and use find_files autocommand to hijack netrw

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -223,8 +223,7 @@ return {
           },
           file_browser = {
             theme = "ivy",
-            -- disables netrw and use telescope-file-browser in its place
-            hijack_netrw = true,
+            hijack_netrw = false,
             mappings = {
               ["i"] = {
                 ["<C-o>"] = fb_actions.goto_parent_dir,
@@ -320,6 +319,37 @@ return {
       telescope.load_extension("hierarchy")
 
       vim.api.nvim_create_user_command('Diagnostics', function() require('telescope.builtin').diagnostics() end, {})
+
+      -- https://github.com/nvim-telescope/telescope.nvim/issues/2806#issuecomment-1904877188
+      local find_files_hijack_netrw = vim.api.nvim_create_augroup("find_files_hijack_netrw", { clear = true })
+      -- clear FileExplorer appropriately to prevent netrw from launching on folders
+      -- netrw may or may not be loaded before telescope-find-files
+      -- conceptual credits to nvim-tree and telescope-file-browser
+      vim.api.nvim_create_autocmd("VimEnter", {
+        pattern = "*",
+        once = true,
+        callback = function()
+          pcall(vim.api.nvim_clear_autocmds, { group = "FileExplorer" })
+        end,
+      })
+      vim.api.nvim_create_autocmd("BufEnter", {
+        group = find_files_hijack_netrw,
+        pattern = "*",
+        callback = function()
+          vim.schedule(function()
+            -- Early return if netrw or not a directory
+            if vim.bo[0].filetype == "netrw" or vim.fn.isdirectory(vim.fn.expand("%:p")) == 0 then
+              return
+            end
+
+            vim.api.nvim_set_option_value('bufhidden', 'wipe', { buf = 0 })
+
+            require("telescope.builtin").find_files({
+              cwd = vim.fn.expand("%:p:h"),
+            })
+          end)
+        end,
+      })
     end,
   },
 }
