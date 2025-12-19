41d90ee5956bc894a7d31b682af595178019a074
Chance Zibolski <chance.zibolski@gmail.com>
2025-12-19 08:57:08 -0800

    neovim: Replace remote-sshfs.nvim with sshfs.nvim

--- neovim/.config/nvim/lua/plugins/ssh.lua
+++ neovim/.config/nvim/lua/plugins/ssh.lua
@@ -1,21 +1,4 @@
 return {
-  "nosduco/remote-sshfs.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-  opts = {
-    handlers = {
-      on_connect = {
-        change_dir = true, -- when connected change vim working directory to mount point
-      },
-    },
-    ui = {
-      confirm = {
-        connect = false,
-        change_dir = false,
-      },
-    },
-  },
-  config = function(_, opts)
-    require('remote-sshfs').setup(opts)
-    require('telescope').load_extension 'remote-sshfs'
-  end
+  "uhs-robert/sshfs.nvim",
+  opts = {},
 }
