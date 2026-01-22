61b1bffa0cb949be0c5ffe87d9444d35a37b1d6b
Chance Zibolski <chance.zibolski@gmail.com>
2026-01-22 10:54:04 -0800

    neovim: Save quickfix list in session

--- neovim/.config/nvim/lua/plugins/session.lua
+++ neovim/.config/nvim/lua/plugins/session.lua
@@ -174,6 +174,27 @@ return {
           end
         end
       },
+      -- Save quickfix list and open it when restoring the session
+      save_extra_cmds = {
+        function()
+          local qflist = vim.fn.getqflist()
+          -- return nil to clear any old qflist
+          if #qflist == 0 then
+            return nil
+          end
+          local qfinfo = vim.fn.getqflist({ title = 1 })
+
+          for _, entry in ipairs(qflist) do
+            -- use filename instead of bufnr so it can be reloaded
+            entry.filename = vim.api.nvim_buf_get_name(entry.bufnr)
+            entry.bufnr = nil
+          end
+
+          local setqflist = "call setqflist(" .. vim.fn.string(qflist) .. ")"
+          local setqfinfo = 'call setqflist([], "a", ' .. vim.fn.string(qfinfo) .. ")"
+          return { setqflist, setqfinfo, "copen" }
+        end,
+      },
     },
   },
 }
