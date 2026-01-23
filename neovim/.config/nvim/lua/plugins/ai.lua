7b88c1590622d66530be9b71ed878adf99a2120f
Chance Zibolski <chance.zibolski@gmail.com>
2026-01-23 09:10:01 -0800

    neovim: Remove opencode.nvim

--- neovim/.config/nvim/lua/plugins/ai.lua
+++ neovim/.config/nvim/lua/plugins/ai.lua
@@ -28,42 +28,6 @@ return {
     }
   },
   {
-    "NickvanDyke/opencode.nvim",
-    dependencies = {
-      -- Recommended for `ask()` and `select()`.
-      -- Required for `toggle()`.
-      -- { "folke/snacks.nvim", opts = { input = {}, picker = {} } },
-    },
-    config = function()
-      vim.g.opencode_opts = {
-        -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on `opencode_opts`.
-      }
-
-      -- Recommended/example keymaps.
-      vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end,
-        { desc = "Ask about this" })
-      vim.keymap.set({ "n", "x" }, "<leader>os", function() require("opencode").select() end, { desc = "Select prompt" })
-      vim.keymap.set({ "n", "x" }, "<leader>o+", function() require("opencode").prompt("@this") end,
-        { desc = "Add this line" })
-      vim.keymap.set({ "n", "x" }, "<leader>of", function() require("opencode").prompt("@buffer") end,
-        { desc = "Add this buffer" })
-      vim.keymap.set({ "n", "x" }, "<leader>ofs", function() require("opencode").prompt("@buffers") end,
-        { desc = "Add open buffers" })
-      vim.keymap.set("n", "<leader>ot", function() require("opencode").toggle() end, { desc = "Toggle embedded" })
-      vim.keymap.set("n", "<leader>oc", function() require("opencode").command() end, { desc = "Select command" })
-      vim.keymap.set("n", "<leader>on", function() require("opencode").command("session_new") end,
-        { desc = "New session" })
-      vim.keymap.set("n", "<leader>oi", function() require("opencode").command("session_interrupt") end,
-        { desc = "Interrupt session" })
-      vim.keymap.set("n", "<leader>oA", function() require("opencode").command("agent_cycle") end,
-        { desc = "Cycle selected agent" })
-      vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("messages_half_page_up") end,
-        { desc = "Messages half page up" })
-      vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("messages_half_page_down") end,
-        { desc = "Messages half page down" })
-    end,
-  },
-  {
     "carlos-algms/agentic.nvim",
     opts = {
       -- provider = "codex-acp",
@@ -155,4 +119,3 @@ return {
     },
   }
 }
-
