3cbcd047990de61e28a998cd8764224ec8f4d48a
Chance Zibolski <chance.zibolski@gmail.com>
2026-03-14 12:56:13 -0700

    neovim: Configure mappings to navigate to next hunk with gitsigns

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -71,7 +71,39 @@ return {
     }
   },
 
-  { 'lewis6991/gitsigns.nvim',      event = "VeryLazy", dependencies = { 'nvim-lua/plenary.nvim' }, opts = {} },
+  {
+    'lewis6991/gitsigns.nvim',
+    event = "VeryLazy",
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']g', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ ']c', bang = true })
+          else
+            gitsigns.nav_hunk('next', { target = 'all' })
+          end
+        end, { desc = 'Next hunk' })
+
+        map('n', '[g', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ '[c', bang = true })
+          else
+            gitsigns.nav_hunk('prev', { target = 'all' })
+          end
+        end, { desc = 'Previous hunk' })
+      end,
+    },
+  },
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
