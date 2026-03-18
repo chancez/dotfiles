fcf8dbc61129cedfc6784edc4c9c66a4694acb49
Chance Zibolski <chance.zibolski@gmail.com>
2026-03-18 09:46:37 -0700

    neovim: Update SQL injection query to handle comments

--- neovim/.config/nvim/queries/go/injections.scm
+++ neovim/.config/nvim/queries/go/injections.scm
@@ -10,6 +10,6 @@
 ; Injects SQL into Go raw string literals that start with SQL keywords.
 (raw_string_literal
   (raw_string_literal_content) @injection.content
-  (#match? @injection.content "^[ \t\n]*(SELECT|INSERT|UPDATE|DELETE|CREATE|ALTER|DROP|WITH|MERGE|TRUNCATE|REPLACE|GRANT|REVOKE)")
+  (#match? @injection.content "^[ \t\n]*(--[^\n]*\n[ \t\n]*)*(SELECT|INSERT|UPDATE|DELETE|CREATE|ALTER|DROP|WITH|MERGE|TRUNCATE|REPLACE|GRANT|REVOKE)")
   (#set! injection.language "sql")
 )
