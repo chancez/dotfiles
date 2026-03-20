3536ac2b70887bd6b50bda44a9b8546ca327b7f4
Chance Zibolski <chance.zibolski@gmail.com>
2026-03-19 20:48:53 -0700

    neovim: Move lualine to it's own file

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -13,41 +13,6 @@ return {
   },
   { 'kyazdani42/nvim-web-devicons', lazy = true },
 
-  {
-    'nvim-lualine/lualine.nvim',
-    event = "VeryLazy",
-    dependencies = { 'kyazdani42/nvim-web-devicons', lazy = true },
-    opts = {
-      options = { theme = 'onedark' },
-      extensions = { 'toggleterm' },
-      sections = {
-        lualine_a = { 'mode', 'g:viewport_active_mode' },
-        lualine_b = { 'diagnostics' },
-        lualine_c = {
-          {
-            'filename',
-            path = 1,
-          }
-        },
-        lualine_x = { 'filetype', 'lsp_status' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-      },
-      tabline = {
-        lualine_a = {
-          {
-            'buffers',
-            mode = 4,
-          }
-        },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = { 'tabs' }
-      }
-    }
-  },
 
   {
     'tadaa/vimade',
