f7a0f4ca0396bc3ca49da484f4c392aa2159fe8b
Chance Zibolski <chance.zibolski@gmail.com>
2026-06-08 10:41:24 -0700

    zsh: Fix annoying issue where pressing "v" in normal mode with an empty command
    When I press "v" in normal mode with an empty command line, it opens an
    empty editor. This is annoying because I often want to just type "vim"
    from normal mode, and instead of doing that it opens an empty editor.
    With this change, if the command line is empty (ignoring whitespace),
    pressing "v" will just put me in insert mode and type "v", so I can
    immediately type "vim" without having to exit the editor first.

--- zsh/.config/zsh/bindings.zsh
+++ zsh/.config/zsh/bindings.zsh
@@ -3,10 +3,22 @@
 # Use human-friendly identifiers.
 zmodload zsh/terminfo
 
-# open the currently entered command in a text editor using 'v' in normal mode
+# open the currently entered command in a text editor using 'v' in normal mode,
+# but when the command line is empty fall through to insert mode and type the 'v'
+# (so typing "vim" from normal mode just works instead of opening an empty editor)
 autoload edit-command-line
 zle -N edit-command-line
-bindkey -M vicmd v edit-command-line
+edit-command-line-or-insert() {
+  # ignore leading/trailing whitespace so a buffer of only spaces counts as empty
+  if [[ -n "${BUFFER//[[:space:]]/}" ]]; then
+    zle edit-command-line
+  else
+    zle vi-insert
+    zle self-insert
+  fi
+}
+zle -N edit-command-line-or-insert
+bindkey -M vicmd v edit-command-line-or-insert
 
 # Copy the current command line to clipboard with Ctrl-Y (without the trailing newline)
 cmd_to_clip() { print -n -- "$BUFFER" | pbcopy }
