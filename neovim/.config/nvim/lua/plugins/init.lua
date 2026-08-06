77f6b7a7d4a2c7b80e6cf1447f6af376bf219595
Chance Zibolski <chance.zibolski@gmail.com>
2026-08-06 10:49:59 -0700

    neovim: Move git plugins to git.lua

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -37,39 +37,6 @@ return {
   },
 
   {
-    'lewis6991/gitsigns.nvim',
-    event = "VeryLazy",
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-      on_attach = function(bufnr)
-        local gitsigns = require('gitsigns')
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Navigation
-        map('n', ']g', function()
-          if vim.wo.diff then
-            vim.cmd.normal({ ']c', bang = true })
-          else
-            gitsigns.nav_hunk('next', { target = 'all' })
-          end
-        end, { desc = 'Next hunk' })
-
-        map('n', '[g', function()
-          if vim.wo.diff then
-            vim.cmd.normal({ '[c', bang = true })
-          else
-            gitsigns.nav_hunk('prev', { target = 'all' })
-          end
-        end, { desc = 'Previous hunk' })
-      end,
-    },
-  },
-  {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     ---@module "ibl"
@@ -201,10 +168,6 @@ return {
     lazy = false,
     version = "*", -- or branch = "main", to use the latest commit
   },
-
-  -- git
-  { 'tpope/vim-fugitive' },
-  { 'tpope/vim-rhubarb' },
 
   -- obsidian
   {
