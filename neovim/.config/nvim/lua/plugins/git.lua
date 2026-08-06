2f60fd091d77c1165c1b25c0a322a3638e9d973c
Chance Zibolski <chance.zibolski@gmail.com>
2026-08-06 16:42:02 -0700

    neovim: Add octo.nvim
    Lazy loads on the Octo command, the leader-o keymaps, and on octo://
    buffers.
    
    For the buffer trigger, use BufReadCmd rather than BufRead. octo:// URIs
    aren't real files, so BufRead never fires for them. BufReadCmd is the
    event octo itself hooks to populate the buffer, and lazy.nvim re-fires
    the triggering event after loading the plugin, so `:e octo://...` both
    loads octo and hands the buffer off to it.

--- neovim/.config/nvim/lua/plugins/git.lua
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -36,4 +36,66 @@ return {
   -- git
   { 'tpope/vim-fugitive' },
   { 'tpope/vim-rhubarb' },
+  {
+    "pwntester/octo.nvim",
+    cmd = "Octo",
+    -- Load on octo:// buffers. octo:// URIs aren't real files, so BufRead never
+    -- fires for them; BufReadCmd is the event octo itself hooks to populate the
+    -- buffer, and lazy re-fires it after loading the plugin.
+    event = "BufReadCmd octo://*",
+    opts = {
+      -- or "fzf-lua" or "snacks" or "default"
+      picker = "telescope",
+      -- bare Octo command opens picker of commands
+      enable_builtin = true,
+      default_remote = { "upstream", "origin", "isovalent" },
+      mappings = {
+        review_diff = {
+          toggle_viewed = { lhs = "<c-space>", desc = "toggle viewer viewed state" },
+        },
+        file_panel = {
+          toggle_viewed = { lhs = "<c-space>", desc = "toggle viewer viewed state" },
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>oi",
+        "<CMD>Octo issue list<CR>",
+        desc = "List GitHub Issues",
+      },
+      {
+        "<leader>op",
+        "<CMD>Octo pr list<CR>",
+        desc = "List GitHub PullRequests",
+      },
+      {
+        "<leader>od",
+        "<CMD>Octo discussion list<CR>",
+        desc = "List GitHub Discussions",
+      },
+      {
+        "<leader>on",
+        "<CMD>Octo notification list<CR>",
+        desc = "List GitHub Notifications",
+      },
+      {
+        "<leader>os",
+        function()
+          require("octo.utils").create_base_search_command { include_current_repo = true }
+        end,
+        desc = "Search GitHub",
+      },
+      {
+        "<leader>or",
+        "<CMD>Octo review<CR>",
+        desc = "Review current PR",
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional if file_panel.icons is a function
+    },
+  }
 }
